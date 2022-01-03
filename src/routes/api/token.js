ca6c5b052897c545e613b8cebe956330d275e269
dirdmaster <kevin.aguilar@me.com>
2022-01-03 14:40:42 +0100

    implements pouya's changes

--- src/routes/api/token.js
+++ src/routes/api/token.js
@@ -6,65 +6,67 @@ const provider = new ethers.providers.JsonRpcProvider(
 );
 const contractAddress = "0x8AdA51404F297bF2603912d1606340223c0a7784";
 const contract = new ethers.Contract(contractAddress, abi, provider);
-const fetchCirculatingSupply = async (data) => {
-  data.initialSupply = "10000000000000.0";
-  await contract
-    .getTotalBurned()
-    .then((r) => (data.totalBurned = ethers.utils.formatUnits(r)));
-  await contract
-    .balanceOf("0x65ef7833898c4a0df62bf150bbaba2718876adf3")
-    .then((r) => (data.dexBalance = ethers.utils.formatUnits(r)));
-  await contract
-    .balanceOf("0x8AdA51404F297bF2603912d1606340223c0a7784")
-    .then((r) => (data.contractBalance = ethers.utils.formatUnits(r)));
-  data.circulatingSupply = (
-    data.initialSupply -
-    data.totalBurned -
-    data.dexBalance -
-    data.contractBalance
-  ).toString();
+
+const fetchCirculatingSupply = async () => {
+  const initialSupply = ethers.BigNumber.from(
+    "10000000000000000000000000000000"
+  );
+  const totalBurned = await contract.getTotalBurned();
+  const dexBalance = await contract.balanceOf(
+    "0x65ef7833898c4a0df62bf150bbaba2718876adf3"
+  );
+  const contractBalance = await contract.balanceOf(
+    "0x8AdA51404F297bF2603912d1606340223c0a7784"
+  );
+
+  const circulatingSupply = initialSupply
+    .sub(totalBurned)
+    .sub(dexBalance)
+    .sub(contractBalance);
+
+  return ethers.utils.formatUnits(circulatingSupply);
 };
+
 const main = async (filter) => {
-  const data = {};
   switch (filter) {
-    case "priceUSD":
-      await fetch(
+    case "priceUSD": {
+      const data = await fetch(
         `https://api.pancakeswap.info/api/v2/tokens/${contractAddress}`
-      )
-        .then((r) => r.json())
-        .then((resData) => (data.priceUSD = resData.data.price));
-      break;
-    case "priceBNB":
-      await fetch(
+      );
+      const response = await data.json();
+
+      return response.data.price;
+    }
+    case "priceBNB": {
+      const data = await fetch(
         `https://api.pancakeswap.info/api/v2/tokens/${contractAddress}`
-      )
-        .then((r) => r.json())
-        .then((resData) => (data.priceBNB = resData.data["price_BNB"]));
-      break;
-    case "circulatingSupply":
-      await fetchCirculatingSupply(data);
-      break;
-    default:
-      await fetch(
+      );
+      const response = await data.json();
+
+      return response.data["price_BNB"];
+    }
+    case "circulatingSupply": {
+      return await fetchCirculatingSupply();
+    }
+    default: {
+      const data = await fetch(
         `https://api.pancakeswap.info/api/v2/tokens/${contractAddress}`
-      )
-        .then((r) => r.json())
-        .then((resData) => {
-          data.price = resData.data.price;
-          data.priceBNB = resData.data["price_BNB"];
-        });
-      await fetchCirculatingSupply(data);
+      );
+      const response = await data.json();
+
+      return {
+        priceUSD: response.data.price,
+        priceBNB: response.data["price_BNB"],
+        circulatingSupply: await fetchCirculatingSupply(),
+      };
+    }
   }
-  return data;
 };
+
 export const get = async (req) => {
   const param = req.query;
   const filter = param.get("filter");
-  if (param.has("filter")) {
-    const data = await main(`${filter}`).then((r) => r);
-    return { body: data[filter] };
-  } else {
-    const data = await main().then((r) => r);
-    return { body: data };
-  }
+  const data = await main(`${filter}`);
+
+  return { body: data };
 };
