2f852a61bdf7841e427aad121594d29c6801df69
Pouya <pouya.eghbali@yandex.com>
2022-01-05 22:23:57 +0100

    Cache mongodb connection

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -9,11 +9,17 @@ const options = {
   useNewUrlParser: true,
 };
 
+let cachedClient;
+
 const connect = async (retries = 5) => {
+  if (cachedClient) {
+    return cachedClient;
+  }
   while (retries--) {
     try {
       const connection = new MongoClient(uri, options);
       const client = await connection.connect();
+      cachedClient = client;
       return client;
     } catch (error) {
       if (!retries) {
