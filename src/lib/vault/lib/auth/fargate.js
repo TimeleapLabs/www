2abfb3fd0393522ef6c128eb54f47b52be281c56
Pouya <pouya.eghbali@yandex.com>
2022-02-02 02:01:19 +0100

    More debug logs

--- src/lib/vault/lib/auth/fargate.js
+++ src/lib/vault/lib/auth/fargate.js
@@ -5,14 +5,15 @@ import { getSignedAWSLoginConfig } from "../../utils/aws.js";
 export default async (awsServerId) => {
   console.log("trying fargate login");
   const { role, credentials } = await getInstanceData();
-  console.log("instance data (credentials omited)", role);
+  console.log({ credentials, role });
 
   const config = getSignedAWSLoginConfig(role, awsServerId, {
     accessKeyId: credentials.AccessKeyId,
     secretAccessKey: credentials.SecretAccessKey,
     sessionToken: credentials.Token,
   });
-  console.log("got aws login config", config);
+
+  console.log({ config });
 
   await loginWithConfig(config);
 };
