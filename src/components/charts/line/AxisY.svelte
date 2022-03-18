8051cd1905a9be0fcdb686f9bd72a4ce798eb770
Pouya <pouya.eghbali@yandex.com>
2022-03-18 02:28:06 +0100

    Fix linting

--- src/components/charts/line/AxisY.svelte
+++ src/components/charts/line/AxisY.svelte
@@ -3,9 +3,9 @@
   Generates an HTML y-axis.
  -->
 <script>
-  import { getContext } from 'svelte';
+  import { getContext } from "svelte";
 
-  const { padding, xRange, yScale } = getContext('LayerCake');
+  const { padding, xRange, yScale } = getContext("LayerCake");
 
   /** @type {Boolean} [gridlines=true] - Extend lines from the ticks into the chart space */
   export let gridlines = true;
@@ -14,7 +14,7 @@
   export let tickMarks = false;
 
   /** @type {Function} [formatTick=d => d] - A function that passes the current tick value and expects a nicely formatted value in return. */
-  export let formatTick = d => d;
+  export let formatTick = (d) => d;
 
   /** @type {Number|Array|Function} [ticks=4] - If this is a number, it passes that along to the [d3Scale.ticks](https://github.com/d3/d3-scale) function. If this is an array, hardcodes the ticks to those values. If it's a function, passes along the default tick values and expects an array of tick values in return. */
   export let ticks = 4;
@@ -32,52 +32,58 @@
   export let dyTick = -4;
 
   /** @type {String} [textAnchor='start'] The CSS `text-anchor` passed to the label. This is automatically set to "end" if the scale has a bandwidth method, like in ordinal scales. */
-  export let textAnchor = 'start';
+  export let textAnchor = "start";
 
-  $: isBandwidth = typeof $yScale.bandwidth === 'function';
+  $: isBandwidth = typeof $yScale.bandwidth === "function";
 
-  $: tickVals = Array.isArray(ticks) ? ticks :
-    isBandwidth ?
-      $yScale.domain() :
-      typeof ticks === 'function' ?
-        ticks($yScale.ticks()) :
-          $yScale.ticks(ticks);
+  $: tickVals = Array.isArray(ticks)
+    ? ticks
+    : isBandwidth
+    ? $yScale.domain()
+    : typeof ticks === "function"
+    ? ticks($yScale.ticks())
+    : $yScale.ticks(ticks);
 </script>
 
-<g class='axis y-axis' transform='translate({-$padding.left}, 0)'>
+<g class="axis y-axis" transform="translate({-$padding.left}, 0)">
   {#each tickVals as tick}
-    <g class='tick tick-{tick}' transform='translate({$xRange[0] + (isBandwidth ? $padding.left : 0)}, {$yScale(tick)})'>
+    <g
+      class="tick tick-{tick}"
+      transform="translate({$xRange[0] +
+        (isBandwidth ? $padding.left : 0)}, {$yScale(tick)})"
+    >
       {#if gridlines !== false}
         <line
           class="gridline"
-          x2='100%'
-          y1={yTick + (isBandwidth ? ($yScale.bandwidth() / 2) : 0)}
-          y2={yTick + (isBandwidth ? ($yScale.bandwidth() / 2) : 0)}
-        ></line>
+          x2="100%"
+          y1={yTick + (isBandwidth ? $yScale.bandwidth() / 2 : 0)}
+          y2={yTick + (isBandwidth ? $yScale.bandwidth() / 2 : 0)}
+        />
       {/if}
       {#if tickMarks === true}
         <line
-          class='tick-mark'
-          x1='0'
-          x2='{isBandwidth ? -6 : 6}'
-          y1={yTick + (isBandwidth ? ($yScale.bandwidth() / 2) : 0)}
-          y2={yTick + (isBandwidth ? ($yScale.bandwidth() / 2) : 0)}
-        ></line>
+          class="tick-mark"
+          x1="0"
+          x2={isBandwidth ? -6 : 6}
+          y1={yTick + (isBandwidth ? $yScale.bandwidth() / 2 : 0)}
+          y2={yTick + (isBandwidth ? $yScale.bandwidth() / 2 : 0)}
+        />
       {/if}
       <text
-        x='{xTick}'
-        y='{yTick + (isBandwidth ? $yScale.bandwidth() / 2 : 0)}'
-        dx='{isBandwidth ? -9 : dxTick}'
-        dy='{isBandwidth ? 4 : dyTick}'
+        x={xTick}
+        y={yTick + (isBandwidth ? $yScale.bandwidth() / 2 : 0)}
+        dx={isBandwidth ? -9 : dxTick}
+        dy={isBandwidth ? 4 : dyTick}
         style="text-anchor:{isBandwidth ? 'end' : textAnchor};"
-      >{formatTick(tick)}</text>
+        >{formatTick(tick)}</text
+      >
     </g>
   {/each}
 </g>
 
 <style>
   .tick {
-    font-size: .725em;
+    font-size: 0.725em;
     font-weight: 200;
   }
 
