03440947cc7fa6b4a066d4c7337c43b85f3f166a
Pouya <pouya.eghbali@yandex.com>
2023-01-24 22:41:31 +0100

    Improve blog/docs seo tags

--- cadey/seo.js
+++ cadey/seo.js
@@ -59,10 +59,12 @@ export const getSeoTags = (body, context, type) => {
     <meta name="title" content="${title}" />
     <meta name="description" content="${description}"/>
 
+    <meta property="og:type" content="website" />
     <meta property="og:title" content="${title}" />
     <meta property="og:description" content="${description}"/>
     <meta property="og:image" content="${absolute(image)}" />
     
+    <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:title" content="${title}" />
     <meta property="twitter:description" content="${description}"/>
     <meta property="twitter:image" content="${absolute(image)}"/>
