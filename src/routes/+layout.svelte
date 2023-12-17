2f9d66f408165a11e9f3ed1424f4f11308d15ddd
Pouya <pouya@kenshi.io>
2023-12-17 19:51:29 +0100

    Add cookie notice

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,17 @@
     document.documentElement.setAttribute("theme", theme);
   }
 
-  onMount(setRef);
+  let cookieMessage = false;
+
+  const acceptCookies = () => {
+    cookieMessage = false;
+    localStorage.setItem("cookieAccepted", "true");
+  };
+
+  onMount(() => {
+    setRef();
+    cookieMessage = localStorage.getItem("cookieAccepted") !== "true";
+  });
 </script>
 
 <Navbar />
@@ -35,3 +45,36 @@
 {#if needsHead}
   <DefaultTags />
 {/if}
+
+{#if cookieMessage}
+  <div class="cookie">
+    <InlineNotification
+      kind="info"
+      subtitle="This website uses cookies"
+      hideCloseButton
+    >
+      <svelte:fragment slot="actions">
+        <NotificationActionButton on:click={acceptCookies}>
+          Accept
+        </NotificationActionButton>
+        <NotificationActionButton
+          href="https://en.wikipedia.org/wiki/HTTP_cookie"
+        >
+          Leave
+        </NotificationActionButton>
+      </svelte:fragment>
+    </InlineNotification>
+  </div>
+{/if}
+
+<style>
+  .cookie {
+    position: fixed;
+    right: 1em;
+    bottom: 1em;
+    max-width: 80%;
+  }
+  .cookie :global(a) {
+    text-decoration: none;
+  }
+</style>
