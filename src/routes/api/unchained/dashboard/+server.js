6e65eb1dd1c6542f2cfc2eee1277afc50badf1ab
Pouya <pouya@kenshi.io>
2024-01-05 18:53:49 +0100

    Disable the scores table

--- src/routes/api/unchained/dashboard/+server.js
+++ src/routes/api/unchained/dashboard/+server.js
@@ -1,26 +1,33 @@
 import { getUnchainedDbClient } from "$lib/unchained/db";
 import { json } from "@sveltejs/kit";
 
+const cache = new Map();
+
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET() {
   const prisma = getUnchainedDbClient();
 
-  const scores = await prisma.signer.findMany({
-    select: {
-      id: true,
-      name: true,
-      key: true,
-      _count: {
-        select: {
-          signersOnAssetPrice: true,
-        },
-      },
-    },
-  });
+  // const scores = await prisma.signer.findMany({
+  //   select: {
+  //     id: true,
+  //     name: true,
+  //     key: true,
+  //     _count: {
+  //       select: {
+  //         signersOnAssetPrice: true,
+  //       },
+  //     },
+  //   },
+  // });
 
-  const signers = await prisma.signer.count();
-  const points = await prisma.assetPrice.count();
-  const validations = await prisma.signersOnAssetPrice.count();
+  const signers = cache.get("signers") || (await prisma.signer.count());
+  const points = cache.get("points") || (await prisma.assetPrice.count());
+  const validations =
+    cache.get("validations") || (await prisma.signersOnAssetPrice.count());
+
+  cache.set("signers", signers);
+  cache.set("points", points);
+  cache.set("validations", validations);
 
   const twoDaysAgo = 2 * 7200;
 
@@ -37,12 +44,12 @@ export async function GET() {
   });
 
   return json({
-    scores: scores.map((score) => ({
-      id: score.id,
-      name: score.name,
-      key: score.key,
-      score: score._count.signersOnAssetPrice,
-    })),
+    // scores: scores.map((score) => ({
+    //   id: score.id,
+    //   name: score.name,
+    //   key: score.key,
+    //   score: score._count.signersOnAssetPrice,
+    // })),
     prices: prices.map((price) => ({
       price: price.price,
       block: price.block,
