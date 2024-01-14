975f04254be7ffc82475015f138c08ef5e05a284
Pouya <pouya@kenshi.io>
2024-01-14 14:04:48 +0100

    Add config short link

--- src/lib/links.js
+++ src/lib/links.js
@@ -1,3 +1,4 @@
 export default {
   cgon: "/blog/how-to-make-a-coingecko-price-oracle-using-the-kenshi-oracle-network",
+  conf: "https://github.com/KenshiTech/unchained/blob/master/quickstart.md#configuration",
 };
