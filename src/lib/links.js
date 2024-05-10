99dfb062d6483e0909ee56cd5b60529ac26f71ef
Roshak Noorani <msnooraniz@gmail.com>
2024-05-10 17:28:58 +0330

    Add node shortlink

--- src/lib/links.js
+++ src/lib/links.js
@@ -1,5 +1,5 @@
 export default {
   cgon: "/blog/how-to-make-a-coingecko-price-oracle-using-the-kenshi-oracle-network",
   conf: "https://github.com/TimeleapLabs/unchained/blob/master/quickstart.md#configuration",
-  runanode: "https://github.com/TimeleapLabs/unchained/blob/master/quickstart.md",
+  node: "https://github.com/TimeleapLabs/unchained/blob/master/quickstart.md",
 };
