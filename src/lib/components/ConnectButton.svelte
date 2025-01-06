306df83b5ba99d800ece2c53e1d4d4d794526338
Hamed Malek Mohammadi <hamedmlk.mohammadi@gmail.com>
2025-01-06 16:23:52 +0330

    WEB-31: add onboard wallet connection (#78)
    * add onboard wallet connection
    
    * Update dapp url typo
    
    * Add connect button to navbar
    
    * Fix wallet connect project ID env
    
    ---------
    
    Co-authored-by: Pouya <pouya@kenshi.io>

---
+++ src/lib/components/ConnectButton.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { onboard } from '$lib/onboard';
+
+	import { wallet, type Wallet } from '$lib/stores/wallet';
+	import Icon from '@iconify/svelte';
+	import { Button } from '@timeleap/ui';
+	import { onMount } from 'svelte';
+
+	const connect = async (): Promise<void> => {
+		if ($wallet?.provider) {
+			await onboard.disconnectWallet({ label: $wallet.label ?? '' });
+		} else {
+			await onboard.connectWallet();
+		}
+	};
+
+	// Typing the subscription
+	const wallets = onboard.state.select('wallets') as {
+		subscribe: (callback: (update: Wallet[]) => void) => { unsubscribe: () => void };
+	};
+
+	// Unsubscribe function
+	let unsubscribe: () => void;
+
+	onMount(() => {
+		unsubscribe = wallets.subscribe((update: Wallet[]) => ([$wallet] = update)).unsubscribe;
+		return () => {
+			try {
+				unsubscribe();
+			} catch (error) {
+				console.error('Error during unsubscribe:', error);
+			}
+		};
+	});
+
+	const formatWalletAddress = (wallet: Wallet): string => {
+		const address = wallet.accounts?.[0].address ?? '';
+		return address.slice(0, 6);
+	};
+</script>
+
+<Button class={$$props.class || ''} on:click={connect}>
+	{#if $wallet?.provider}
+		<span class="text-zinc-400">{formatWalletAddress($wallet)}</span> — <span>Disconnect</span>
+	{:else}
+		Connect <Icon icon="material-symbols:account-balance-wallet" />
+	{/if}
+</Button>
---
+++ src/lib/onboard.ts
@@ -0,0 +1,48 @@
+import walletConnectModule from '@web3-onboard/walletconnect';
+import Onboard from '@web3-onboard/core';
+import injectedModule from '@web3-onboard/injected-wallets';
+import { PUBLIC_WALLET_CONNECT_PROJECT_ID } from '$env/static/public';
+
+const walletConnectProjectId = PUBLIC_WALLET_CONNECT_PROJECT_ID;
+
+const appName = 'Timeleap';
+
+const injected = injectedModule();
+const walletConnect = walletConnectModule({
+	projectId: walletConnectProjectId,
+	requiredChains: [0xa4b1],
+	dappUrl: 'https://timeleap.swiss/'
+});
+
+const options = {
+	appMetadata: {
+		name: appName,
+		description: 'Timeleap dApp',
+		icon: 'images/tl-white.svg'
+	},
+	accountCenter: {
+		desktop: {
+			enabled: false
+		},
+		mobile: {
+			enabled: false
+		}
+	},
+	chains: [
+		{
+			id: '0xa4b1',
+			token: 'ETH',
+			label: 'Arbitrum One',
+			rpcUrl: 'https://arbitrum.blockpi.network/v1/rpc/public'
+		},
+		{
+			id: '0x157b',
+			token: 'TLP',
+			label: 'Timeleap Devnet',
+			rpcUrl: 'https://devnet.timeleap.swiss/rpc'
+		}
+	],
+	wallets: [walletConnect, injected]
+};
+
+export const onboard = Onboard(options);
