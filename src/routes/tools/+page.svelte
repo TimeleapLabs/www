13114e94d4bc97b5f8ef4e452845aa20fcebdf50
Kevin Aguilar <kevin@timeleap.swiss>
2025-03-28 14:33:52 +0100

    Improve tools page

--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -3,21 +3,17 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import WalletTools from '$lib/components/tools/WalletTools.svelte';
 	import WalletStatistics from '$lib/components/tools/WalletStatistics.svelte';
-	import { Card, Grid, Section } from '@timeleap/ui';
+	import { Card, Section } from '@timeleap/ui';
 	import kenshiAbi from '$lib/abi/kenshi';
-	import { page } from '$app/stores';
 	import { ethers } from 'ethers';
 	import { fetchTokenPriceFromPair } from '$lib/api/token';
 	import { wallet } from '$lib/stores/wallet';
 	import { onMount } from 'svelte';
 
-	let userAddress = $page?.url?.searchParams?.get?.('address') ?? undefined;
-
 	let balance: bigint;
 	let unitPrice: number;
-	let treasury: bigint;
-	let reserve: bigint;
 	let staked: bigint;
+	let checksumAddress: string;
 
 	const formatCurrency = (n: string | bigint | number, unit = '') => {
 		return unit + formatThousands(trimDecimals(n), ',');
@@ -41,18 +37,12 @@
 	};
 
 	$: balanceDisplay = toKenshi(balance || 0n);
-	$: treasuryDisplay = toKenshi(treasury || 0n);
-	$: reserveDisplay = toKenshi(reserve || 0n);
 	$: stakedDisplay = toKenshi(staked || 0n);
 
 	$: usdBalanceDisplay = balance && unitPrice ? toUsd(balance) : 0n;
-	$: usdTreasuryDisplay = treasury && unitPrice ? toUsd(treasury) : 0n;
-	$: usdReserveDisplay = reserve && unitPrice ? toUsd(reserve) : 0n;
 	$: usdStakedDisplay = staked && unitPrice ? toUsd(staked) : 0n;
 
 	const kenshiAddr = '0xf1264873436A0771E440E2b28072FAfcC5EEBd01';
-	const treasuryAddr = '0xD59321c8266534dac369F0eFABDD5b815F1a5eb6';
-	const reserveAddr = '0x51DD193630806aDCFFa9E72569a71A9c12591C33';
 	const stakeAddr = '0xE894BD5Ec531EC8AAe856AFC3E0Fc948Ab22Efb4';
 	const jsonRpcUrl = 'https://arbitrum.blockpi.network/v1/rpc/public';
 	const provider = new ethers.JsonRpcProvider(jsonRpcUrl);
@@ -60,7 +50,7 @@
 
 	const onWallet = async () => {
 		const walletProvider = new ethers.BrowserProvider($wallet?.provider);
-		userAddress = await (await walletProvider.getSigner()).getAddress();
+		checksumAddress = await (await walletProvider.getSigner()).getAddress();
 		updateValues();
 	};
 
@@ -70,16 +60,17 @@
 	};
 
 	const updateValues = async () => {
-		if (!userAddress) return;
-		balance = await contract.balanceOf(userAddress);
-		treasury = await contract.balanceOf(treasuryAddr);
-		reserve = await contract.balanceOf(reserveAddr);
+		if (!checksumAddress) return;
+
+		balance = await contract.balanceOf(checksumAddress);
 		staked = await contract.balanceOf(stakeAddr);
 	};
 
 	$: if ($wallet?.provider) onWallet();
-	$: if (userAddress && unitPrice) updateValues().catch(() => null);
-
+	$: {
+		if (ethers.isAddress(checksumAddress)) updateValues();
+		else balance = 0n;
+	}
 	onMount(() => {
 		updatePrice();
 		const priceInterval = setInterval(updatePrice, 5 * 60 * 1000);
@@ -93,27 +84,20 @@
 
 <Navbar active="none"></Navbar>
 
-<Section class="w-full max-w-[1280px] mx-auto pt-20 pb-24 flex-1 !gap-16 px-4 md:px-16 xxl:px-0">
-	<h2 class="text-white font-serif text-5xl mt-4">Timeleap Tools</h2>
+<Section class="md:p-10 p-2">
+	<h2 class="relative py-5 md:top-10 top-13 text-white font-serif text-5xl">Timeleap Tools</h2>
 
-	<Card class="bg-zinc-900 text-white p-8 rounded-lg shadow-lg">
-		<Grid extraLargeScreenColumns={2} largeScreenColumns={2}>
-			<!-- Left Column: Wallet Tools -->
+	<Card class="bg-zinc-900 text-white md:p-8 rounded-lg shadow-lg">
+		<div class="flex flex-wrap md:p-5 gap-10">
 			<WalletTools />
-
-			<!-- Right Column: Wallet Statistics -->
 			<WalletStatistics
-				{userAddress}
+				bind:checksumAddress
 				{balanceDisplay}
 				{usdBalanceDisplay}
-				{treasuryDisplay}
-				{usdTreasuryDisplay}
-				{reserveDisplay}
-				{usdReserveDisplay}
 				{stakedDisplay}
 				{usdStakedDisplay}
 			/>
-		</Grid>
+		</div>
 	</Card>
 </Section>
 
