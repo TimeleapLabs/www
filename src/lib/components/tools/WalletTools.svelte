d546488fd38a2dd1b0a6965f56f8720b120122f3
Kevin Aguilar <kevin@timeleap.swiss>
2025-03-28 14:21:48 +0100

    Improve WalletTools

--- src/lib/components/tools/WalletTools.svelte
+++ src/lib/components/tools/WalletTools.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { Button } from '@timeleap/ui';
 	import ConnectButton from '$lib/components/ConnectButton.svelte';
-	import { CreditCard, Wallet, ExternalLinkIcon } from 'lucide-svelte';
+	import { CreditCard, ExternalLinkIcon } from 'lucide-svelte';
 	import toast from 'svelte-french-toast';
 	import { onboard } from '$lib/onboard';
 	import { wallet } from '$lib/stores/wallet';
+	import type { ComponentType } from 'svelte';
+	import MetamaskIcon from '../icons/MetamaskIcon.svelte';
 
 	const addToMetamask = async () => {
 		const params = {
@@ -17,49 +19,75 @@
 			}
 		};
 
+		if (!$wallet) await onboard.connectWallet();
+
 		try {
 			await onboard.setChain({ chainId: '0xa4b1' });
-		} catch (error) {
-			return toast.error("Couldn't switch to the Arbitrum One network.");
+		} catch (error: unknown) {
+			toast.error("Couldn't switch to the Arbitrum One network.");
+
+			if (error instanceof Error) throw new Error(error.message);
+			throw new Error(String(error));
 		}
 
-		if ($wallet)
-			$wallet.provider.request({ method: 'wallet_watchAsset', params }).catch((error: any) => {
-				console.error('Error adding token to wallet:', error);
-				toast.error("Couldn't add the token to your wallet.");
-			});
+		try {
+			await $wallet?.provider.request({ method: 'wallet_watchAsset', params });
+		} catch (error: unknown) {
+			toast.error("Couldn't add the token to your wallet.");
+
+			if (error instanceof Error) throw new Error(error.message);
+			throw new Error(String(error));
+		}
 	};
+	interface Links {
+		name: string;
+		label: string;
+		icon: ComponentType;
+		url?: string;
+		action?: () => Promise<void>;
+	}
+	const links: Links[] = [
+		{
+			name: 'uniswap',
+			label: 'Buy ₭enshi',
+			icon: CreditCard,
+			url: 'https://app.uniswap.org/swap?outputCurrency=0xf1264873436A0771E440E2b28072FAfcC5EEBd01'
+		},
+		{
+			name: 'metamask',
+			label: 'Add ₭enshi',
+			icon: MetamaskIcon,
+			action: async () => await addToMetamask()
+		},
+		{
+			name: 'dexscreener',
+			label: 'Charts',
+			icon: ExternalLinkIcon,
+			url: 'https://dexscreener.com/arbitrum/0x68c685fd52a56f04665b491d491355a624540e85'
+		}
+	];
 </script>
 
-<div class="flex flex-col gap-4">
+<div class="grow-2 flex flex-col gap-4">
 	<p>
-		{$wallet?.provider
-			? 'You are connected as'
-			: "You'll need to connect your wallet to use certain areas of Timeleap tools."}
+		{#if $wallet?.provider}
+			You are connected as
+		{:else}
+			You'll need to connect your wallet to use certain areas of Timeleap tools.
+		{/if}
 	</p>
 	<ConnectButton class="bg-zinc-800 hover:bg-zinc-700" />
-	<div class="buttons flex gap-4">
-		<Button
-			href="https://app.uniswap.org/swap?outputCurrency=0xf1264873436A0771E440E2b28072FAfcC5EEBd01"
-			class="bg-zinc-800 hover:bg-zinc-700"
-		>
-			Buy Kenshi <CreditCard size={'1em'} strokeWidth={2.5} />
-		</Button>
-		<Button on:click={addToMetamask} class="bg-zinc-800 hover:bg-zinc-700">
-			Add ₭enshi <Wallet />
-		</Button>
-		<Button
-			href="https://dexscreener.com/arbitrum/0x68c685fd52a56f04665b491d491355a624540e85"
-			class="bg-zinc-800 hover:bg-zinc-700"
-		>
-			Charts <ExternalLinkIcon size={'1em'} strokeWidth={2.5} />
-		</Button>
+	<div class="buttons flex flex-wrap gap-4">
+		{#each links as link}
+			<Button
+				href={link?.url}
+				target="_blank"
+				on:click={link?.action}
+				class="bg-zinc-800 hover:bg-zinc-700 grow"
+			>
+				<svelte:component this={link.icon} size={'1em'} />
+				{link.label}
+			</Button>
+		{/each}
 	</div>
-
-	<p class="text-sm text-zinc-400 mt-4">
-		What you should know about the TLP token: <a
-			href="/blog/rebrand"
-			class="text-blue-500 hover:underline">Read more</a
-		>
-	</p>
 </div>
