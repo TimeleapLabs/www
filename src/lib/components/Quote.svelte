6dfdcd3f72f145c2927d7fe16ac0ea60b6b4867f
Hamedblue1381 <hamed.balanar@gmail.com>
2025-07-30 14:17:47 +0330

    feat: add quote component

---
+++ src/lib/components/Quote.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let content = '';
+	export let author = '';
+</script>
+
+<aside
+	class="min-h-10 flex flex-col justify-between border-l-4 border-neutral-400 pl-4 italic text-neutral-300 p-4"
+	style="position: relative;"
+>
+	<p>“ {content}”</p>
+	{#if author}
+		<p class="text-right mt-2 text-sm font-semibold" style="align-self: flex-end;">— {author}</p>
+	{/if}
+</aside>
